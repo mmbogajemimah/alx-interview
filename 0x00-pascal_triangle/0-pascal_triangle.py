75db027bbaab3484d765bab28451701a818895eb
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-01-25 23:12:15 +0300

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,11 +2,15 @@
 """
 pascal_triangle function
 """
+
+
 def pascal_triangle(n):
     """
-    returns a list of lists of integers representing the Pascal’s triangle 
+    returns a list of lists of integers
+    representing the Pascal’s triangle 
     """
-    if n <= 0:
+
+    if (n <= 0):
         return []
     triangle = []
     for i in range(n):
@@ -14,4 +18,5 @@ def pascal_triangle(n):
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
+
     return triangle
