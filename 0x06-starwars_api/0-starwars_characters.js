1920ec0274d75f5c566fefcf0cb90cf2d573ad3f
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-03-09 23:27:35 +0300

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,46 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+
+async function starWarsApi() {
+    // request(`https://swapi-api.alx-tools.com/api/films/${num}/`, function (error, response, body) {
+    //     console.error('error', error); // Print the error if one occurred
+    //     console.log('statusCode', response && response.statusCode); // Print the response status code if a response was received
+    //     console.log('body', body); // Print the HTML for the Google homepage.
+    // });
+    
+    const num = process.argv[2];
+    const options = {
+        url: `https://swapi-api.alx-tools.com/api/films/${num}/`,
+        json: true
+    };
+
+    request(options, (error, response, body) => {
+        // console.log('statusCode', response && response.statusCode);
+        if(error) {
+            console.error('Error: ', error);
+        } else {
+            //console.log('Character-APIs: ', body.characters);
+            const charactersAPI = body.characters;
+            //console.log(charactersAPI);
+            for (characterAPI in charactersAPI){
+                //console.log(charactersAPI[characterAPI]);
+                const character = {
+                    url: charactersAPI[characterAPI],
+                    json: true
+                }
+                request(character, (error, response, body) => {
+                    if(error) {
+                        console.error('Error:', error);
+                    } else {
+                        console.log('Character name:', body.name)
+                    }
+                })
+            }
+        }
+    })
+}
+
+starWarsApi();
+
