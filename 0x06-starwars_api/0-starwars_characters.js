9ed813782bb2bf122d422584df3596752411b6ad
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-03-13 00:44:02 +0300

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,52 @@
 #!/usr/bin/node
 
 const request = require('request');
+const filmId = process.argv[2];
+const filmAPI = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+let people = [];
+const names = [];
 
-async function starWarsApi () {
-  // request(`https://swapi-api.alx-tools.com/api/films/${num}/`, function (error, response, body) {
-  //     console.error('error', error); // Print the error if one occurred
-  //     console.log('statusCode', response && response.statusCode); // Print the response status code if a response was received
-  //     console.log('body', body); // Print the HTML for the Google homepage.
-  // });
-
-  let characterAPI;
-  const num = process.argv[2];
-  const options = {
-    url: `https://swapi-api.alx-tools.com/api/films/${num}/`,
-    json: true
-  };
-
-  request(options, (error, response, body) => {
-    // console.log('statusCode', response && response.statusCode);
-    if (error) {
-      console.error('Error: ', error);
+const charactersAPI = async () => {
+  await new Promise(resolve => request(filmAPI, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      console.error('Error: ', error, '| StatusCode: ', response.statusCode);
     } else {
-    // console.log('Character-APIs: ', body.characters);
-      const charactersAPI = body.characters;
-      // console.log(charactersAPI);
-      for (characterAPI in charactersAPI) {
-      // console.log(charactersAPI[characterAPI]);
-        const character = {
-          url: charactersAPI[characterAPI],
-          json: true
-        };
-        request(character, (error, response, body) => {
-          if (error) {
-            console.error('Error:', error);
-          } else {
-            console.log(body.name);
-          }
-        });
-      }
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
     }
-  });
-}
+  }));
+};
 
-starWarsApi();
+const characterNames = async () => {
+  if (people.length > 0) {
+    for (const person of people) {
+      await new Promise(resolve => request(person, (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          console.error('Error: ', error, '| StatusCode: ', response.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
+  } else {
+    console.error('Error: Got no characters for some reason');
+  }
+};
+
+const getCharacterNames = async () => {
+  await charactersAPI();
+  await characterNames();
+
+  for (const name of names) {
+    if (name === names[names.length - 1]) {
+      process.stdout.write(name);
+    } else {
+      process.stdout.write(name + '\n');
+    }
+  }
+};
+
+getCharacterNames();
