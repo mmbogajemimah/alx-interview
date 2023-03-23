14de4ec782686331c8aa59071f6018f68da45c40
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-03-23 19:37:09 +0300

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
+"""
+Making Change
+"""
+
+
 def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the
+    fewest number of coins needed to meet a given amount total
+    """
     # Initialize counter
     counter = 0
 
